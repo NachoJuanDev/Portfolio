ac8f53afd51fb599aa5d557101e5179bb632e62d
Juan Salas-Morales <jisalas1@uc.cl>
2024-05-13 11:29:31 -0400

    feat: add tailwind, dark mode and layout

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        // Dark mode
+        dark: '#282a36',
+        'text-white': 'white',
+
+        // Ligth mode
+        ligth: '#F8F8F2',
+        'text-black': 'black',
+      },
+    },
+  },
+  plugins: [],
+}
